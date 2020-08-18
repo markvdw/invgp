996d4fffac5d38c98474531ae38548c16142d508
Martin <marjorg2@gmail.com>
2020-08-18 10:41:50 +0200

    Changed to match test, maybe works for invariant with by uncommenting ?

--- invgp/models/sample_SVGP.py
+++ invgp/models/sample_SVGP.py
@@ -3,7 +3,8 @@ from gpflow.models.model import GPModel, InputData, RegressionData, MeanAndVaria
 import numpy as np
 import tensorflow as tf
 
-
+from gpflow.conditionals import conditional
+from gpflow.conditionals.util import sample_mvn
 class sample_SVGP(SVGP):
     def __init__(
         self,
@@ -18,7 +19,7 @@ class sample_SVGP(SVGP):
         q_sqrt=None,
         whiten: bool = True,
         num_data=None,
-        S_a_S_g: int = 5,
+        S_f: int = 5,
     ):
         """
         - kernel, likelihood, inducing_variables, mean_function are appropriate
@@ -33,7 +34,7 @@ class sample_SVGP(SVGP):
         """
         # init the super class, accept args
         super().__init__(kernel, likelihood, inducing_variable, num_latent_gps=num_latent_gps)
-        self.S_a_S_g = S_a_S_g
+        self.S_f = S_f
 
 
     def elbo(self, data: RegressionData): 
@@ -44,19 +45,19 @@ class sample_SVGP(SVGP):
         """
         X, Y = data # X is [N x D]
         # initilize some dimensions
-        S_o = X.shape[1]
-        N = X.shape[0]
-        D = X.shape[1]
-        P = self.num_latent_gps
+        #S_o = X.shape[1]
+        #N = X.shape[0]
+        #D = X.shape[1]
+        #P = self.num_latent_gps
 
         kl = self.prior_kl()
-        print('X shape is', X.shape)
-        f_samples = self.predict_f_samples(X, num_samples=self.S_a_S_g, full_cov=False, full_output_cov=False) # now: [S_a_S_g, N, P] (later: [S_a, S_g, N, P] ?)
-        print('f_samples shape is', f_samples.shape) 
+        #print('X shape is', X.shape)
+        f_samples = self.predict_f_samples(X, num_samples=self.S_f, full_cov=True, full_output_cov=False) # [S_f, N, P]
+        #print('f_samples shape is', f_samples.shape) 
 
         # expand Y to the right dimensions
         Y = tf.expand_dims(Y, 0) # (1, 50, 10)
-        Y = tf.tile(Y, [self.S_a_S_g, 1, 1])
+        Y = tf.tile(Y, [self.S_f, 1, 1])
         # compute likelihoods
         likelihoods = self.likelihood.log_prob(f_samples, Y)
         # compute expectations over g and X_a
@@ -70,54 +71,33 @@ class sample_SVGP(SVGP):
             scale = tf.cast(1.0, kl.dtype)
         return tf.reduce_sum(exp_likelihood) * scale - kl
 
-
-#     def predict_f_samples(                        # TODO: override from GPmodels
-#        self,
-#        Xnew: InputData,
-#        num_samples: Optional[int] = None,
-#        full_cov: bool = True,
-#        full_output_cov: bool = False,
-#    ) -> tf.Tensor:
-        """
-        Produce samples from the posterior latent function(s) at the input points.
-
-        :param Xnew: InputData
-            Input locations at which to draw samples, shape [..., N, D]
-            where N is the number of rows and D is the input dimension of each point.
-        :param num_samples:
-            Number of samples to draw.
-            If `None`, a single sample is drawn and the return shape is [..., N, P],
-            for any positive integer the return shape contains an extra batch
-            dimension, [..., S, N, P], with S = num_samples and P is the number of outputs.
-        :param full_cov:
-            If True, draw correlated samples over the inputs. Computes the Cholesky over the
-            dense covariance matrix of size [num_data, num_data].
-            If False, draw samples that are uncorrelated over the inputs.
-        :param full_output_cov:
-            If True, draw correlated samples over the outputs.
-            If False, draw samples that are uncorrelated over the outputs.
-
-        Currently, the method does not support `full_output_cov=True` and `full_cov=True`.
-        """
-        # if full_cov and full_output_cov:
-        #     raise NotImplementedError(
-        #         "The combination of both `full_cov` and `full_output_cov` is not supported."
-        #     )
-
-        # # check below for shape info
-        # mean, cov = self.predict_f(Xnew, full_cov=full_cov, full_output_cov=full_output_cov)
-        # if full_cov:
-        #     # mean: [..., N, P]
-        #     # cov: [..., P, N, N]
-        #     mean_for_sample = tf.linalg.adjoint(mean)  # [..., P, N]
-        #     samples = sample_mvn(
-        #         mean_for_sample, cov, full_cov, num_samples=num_samples
-        #     )  # [..., (S), P, N]
-        #     samples = tf.linalg.adjoint(samples)  # [..., (S), N, P]
-        # else:
-        #     # mean: [..., N, P]
-        #     # cov: [..., N, P] or [..., N, P, P]
-        #     samples = sample_mvn(
-        #         mean, cov, full_output_cov, num_samples=num_samples
-        #     )  # [..., (S), N, P]
-        # return samples  # [..., (S), N, P]
+    def predict_f_samples(
+            self,
+            Xnew: InputData,
+            num_samples: int = None,
+            full_cov: bool = True,
+            full_output_cov: bool = False) -> tf.Tensor:
+        
+        
+        #X_o = self.kernel.orbit(Xnew) # [N, C, D]
+        #Xnew = tf.reshape(X_o, [X_o.shape[0]*X_o.shape[1], X_o.shape[2]]) # [NC, D]
+        q_mu = self.q_mu; q_sqrt = self.q_sqrt;
+        mu, var = conditional(
+            Xnew,
+            self.inducing_variable,
+            self.kernel, #.basekern,
+            q_mu,
+            q_sqrt=q_sqrt,
+            full_cov=full_cov,
+            white=self.whiten,
+            full_output_cov=full_output_cov)
+        
+        mu = tf.linalg.adjoint(mu)  # [..., P, NC]
+        samples = sample_mvn(
+            mu, var, full_cov=True, num_samples=num_samples
+        )  # [..., (S), P, NC]
+        samples = tf.linalg.adjoint(samples)  # [..., (S), NC, P]
+        
+        #samples = tf.reshape(samples, [num_samples, X_o.shape[0], X_o.shape[1], samples.shape[2]]) # [S, N, C, P]
+        #samples = tf.reduce_mean(samples, axis = 2) # [S, N, P]
+        return samples #[..., (S), N, P]
