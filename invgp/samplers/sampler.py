d0b1d06d0fbb68462a6bb058905f4d9d19069be1
Martin <marjorg2@gmail.com>
2020-08-23 11:32:01 +0200

    First attempt at Matheron sampler, no proper tests done

--- invgp/samplers/sampler.py
+++ invgp/samplers/sampler.py
@@ -1,10 +1,8 @@
 import tensorflow as tf
 import numpy as np
-from invgp.inducing_variables import ConvolvedInducingPoints
 from invgp.kernels import Invariant
 from gpflow.utilities import Dispatcher
 from gpflow.kernels import Kernel
-from gpflow.inducing_variables import InducingPoints
 from gpflow import kernels
 
 sample_matheron = Dispatcher("sample_matheron")
@@ -33,10 +31,6 @@ def __sample_matheron(Xnew, inducing_variable, kernel, q_mu, q_sqrt, num_samples
     _Z = tf.divide(inducing_variable.Z, kernel.lengthscales)
     
     w = tf.random.normal(shape = [num_samples, q_mu.shape[1], num_basis, 1], dtype = Xnew.dtype) # [S, P, num_basis, 1]
-    print('Xshape:', _Xnew.shape)
-    print('w shape:', w.shape)
-    print('spectrum:', spectrum.shape)
-    print('bias:', bias.shape)
     fX = tf.sqrt(kernel.variance * 2 / num_basis) * tf.cos(tf.matmul(_Xnew, spectrum, transpose_b = True) + bias) # [N, num_basis]
     fX = tf.tile(fX[None,None,:,:], [num_samples, q_mu.shape[1], 1,1] ) @ w  # [S, P, N, 1]
     fZ = tf.sqrt(kernel.variance * 2 / num_basis) * tf.cos(tf.matmul(_Z, spectrum, transpose_b = True) + bias) # [M, num_basis]
@@ -44,14 +38,10 @@ def __sample_matheron(Xnew, inducing_variable, kernel, q_mu, q_sqrt, num_samples
     
     # Update
     v = tf.random.normal(shape = [num_samples, q_mu.shape[1], q_mu.shape[0], 1], dtype = Xnew.dtype) # [S, P, M, 1]
-    print(q_mu.shape)
     _u = tf.tile(tf.linalg.adjoint(q_mu)[None, :, :, None], [num_samples, 1,1,1]) +  tf.matmul(tf.tile(q_sqrt[None,:,:,:], [num_samples,1,1,1]), v)# [S, P, M, 1]
     
     Luu = tf.linalg.cholesky(kernel.K(inducing_variable.Z)) # [M, M]
-    print('Luushape:', Luu.shape)
     res_upd = tf.linalg.cholesky_solve(tf.tile(Luu[None, None, :, :], [num_samples, q_mu.shape[1],1,1]) , _u - fZ) # [S, P, M, 1]
-    print('res_upd shape:', res_upd.shape)
-    print('K shape:', kernel.K(Xnew, inducing_variable.Z)) #[N, M]
     f_upd = kernel.K(Xnew, inducing_variable.Z) @ res_upd # [S, P, N, 1]
     samples = tf.linalg.adjoint( tf.squeeze(fX + f_upd, axis = 3) )
     return samples # [S, N, P]
