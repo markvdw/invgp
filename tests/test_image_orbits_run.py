ff220df81a4fa39b877d634a9f6bdccb18aa71cd
polaschwoebel <posc@dtu.dk>
2020-09-22 13:59:47 +0200

    Learnable ImageRotQuant orbit (#10)
    * more flexible ImageRotQuant orbit
    
    * added test
    
    * ImageRotQuant now supports angle learning
    
    * assertion does not make sense for learned orbit
    
    * ImageRotQuant orbit is now parametrized by orbit size
    
    * learning the angle for ImageRotQuant working
    
    * no - learning the angle for ImageRotQuant not working - backprop through tf.linspace?
    
    * ImageRotQuant, learned angle working
    
    * delete unsused import
    
    * tiny clean up
    
    Co-authored-by: Pola Elisabeth Schwbel <posc@comp-pcposc.compute.dtu.dk>

--- tests/test_image_orbits_run.py
+++ tests/test_image_orbits_run.py
@@ -11,8 +11,19 @@ from invgp_experiments.datasets import load_mnist
     orbits.ImageRotation(90, minibatch_size=10, interpolation_method="NEAREST"),
     orbits.ImageRotation(90, minibatch_size=10, interpolation_method="BILINEAR"),
     orbits.ImageRotation(90, minibatch_size=10, interpolation_method="BILINEAR", use_stn=True),
-    orbits.GeneralSpatialTransform(minibatch_size=10)
+    orbits.GeneralSpatialTransform(minibatch_size=10),
+    orbits.ImageRotQuant(orbit_size=10, angle=90, interpolation_method="NEAREST"),
+    orbits.ImageRotQuant(orbit_size=10, angle=5, interpolation_method="NEAREST"),
 ])
 def test_if_orbit_runs(orbit):
     (X, Y), _ = load_mnist()
     Xo = orbit(X[:2, :])
+
+# A sligtly more advanced test that checks whether the orbit has the right number of images
+@pytest.mark.parametrize("angle", [10, 30, 90])
+def test_flexible_orbit_size(angle):
+    (X, Y), _ = load_mnist()
+    orbit = orbits.ImageRotQuant(orbit_size=10, angle=angle, interpolation_method="NEAREST")
+    Xo = orbit(X[:1, :])
+    orbit_size = Xo.shape[1]
+    assert orbit_size == 10
