0c565ef42ce55400e23336e8e0c7048b07e087dd
Mark van der Wilk <m.vdwilk@imperial.ac.uk>
2021-01-07 12:25:58 +0100

    Added support for colour channel in STN + fixed test. There still is opportunity to optimise for speed.

--- invgp/kernels/image_transforms.py
+++ invgp/kernels/image_transforms.py
@@ -77,7 +77,9 @@ def _stn_theta_vec(thetas, radians=False):
     s = tf.sin(angle_rad)
     c = tf.cos(angle_rad)
 
-    return tf.convert_to_tensor([sx * c - ty * sx * s, tx * sy * c - sy * s, 0., sx * s + ty * sx * c, tx * sy * s + sy * c, 0.], dtype=default_float())
+    return tf.convert_to_tensor(
+        [sx * c - ty * sx * s, tx * sy * c - sy * s, 0., sx * s + ty * sx * c, tx * sy * s + sy * c, 0.],
+        dtype=default_float())
 
 
 def _apply_stn(Ximgs, theta):
@@ -110,4 +112,35 @@ def apply_stn_batch(Ximgs, thetas):
         Ximgs = tf.expand_dims(Ximgs, -1)  # [None, H, W, 1]
 
     result = tf.transpose(tf.map_fn(lambda x: _apply_stn(Ximgs, x), thetas, dtype=Ximgs.dtype), (1, 0, 2))
+    return tf.cast(result, default_float())
+
+
+def apply_stn_batch_colour(Ximgs, thetas):
+    """
+    Use spatial transformer networks to apply a general affine transformation (6 parameters). Every image is transformed
+    with every theta
+    :param Ximgs: input images [None, H, W] or [None, H, W, 1]
+    :param thetas: parameters of the affine transformation by [P, 6]
+    :return: [None, P, H*W]
+    """
+
+    def _apply_stn_colour(Ximgs, theta):
+        """
+        Use spatial transformer networks to apply a general affine transformation (6 parameters). All images are transformed
+        by the SAME theta
+        :param Ximgs: [None, H, W, 1]
+        :param theta: [6]
+        :return: [None, H, W, 1]
+        """
+        theta = tf.reshape(theta, [1, -1])
+        theta = tf.tile(theta, [tf.shape(Ximgs)[0], 1])
+        return stn(Ximgs, theta)
+
+    Ximgs = tf.cast(Ximgs, tf.float32)
+    thetas = tf.cast(thetas, tf.float32)
+
+    if len(Ximgs.get_shape()) == 3:
+        Ximgs = tf.expand_dims(Ximgs, -1)  # [None, H, W, 1]
+
+    result = tf.transpose(tf.map_fn(lambda x: _apply_stn_colour(Ximgs, x), thetas, dtype=Ximgs.dtype), (1, 0, 2, 3, 4))
     return tf.cast(result, default_float())
