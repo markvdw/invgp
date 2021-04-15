be53b24b52cd570e5d9228973632b147ab8dc1cd
Pola Elisabeth Schwobel <posc@dtu.dk>
2021-04-15 14:05:15 +0200

    adjusted visual_checks.py to new 7 param orbit

--- tests/visual_checks.py
+++ tests/visual_checks.py
@@ -7,7 +7,10 @@ import tensorflow as tf
 
 # parametrization: [angle_deg, sx, sy, tx, ty], [0., 1., 1., 0., 0.]
 
-orbit = orbits.InterpretableSpatialTransform(minibatch_size=10, theta_min=[0., 0.5, 0.5, 0., 0.], theta_max=[0., 2., 2., 0., 0.])
+orbit = orbits.InterpretableSpatialTransform(
+    minibatch_size=10,
+    theta_min=[0., 0.5, 0.5, 0., 0., 0., 0.],
+    theta_max=[0., 2., 2., 0., 0., 0., 0.])
 Xo = orbit(X[:2, :])
 
 fig, ax = plt.subplots(11, 2)
@@ -16,6 +19,6 @@ ax[0, 0].imshow(tf.reshape(X[0], (28, 28)), cmap='gray')
 ax[0, 1].imshow(tf.reshape(X[1], (28, 28)), cmap='gray')
 
 for i in range(1, 11):
-	ax[i, 0].imshow(tf.reshape(Xo[0, i-1, :], (28, 28)), cmap='gray')
-	ax[i, 1].imshow(tf.reshape(Xo[1, i-1, :], (28, 28)), cmap='gray')
+    ax[i, 0].imshow(tf.reshape(Xo[0, i-1, :], (28, 28)), cmap='gray')
+    ax[i, 1].imshow(tf.reshape(Xo[1, i-1, :], (28, 28)), cmap='gray')
 plt.show()
