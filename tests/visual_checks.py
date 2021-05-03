f0f42cb7b9b38f9f0eeadbe5d433a513fc668ee7
Pola Elisabeth Schwobel <posc@dtu.dk>
2021-05-03 09:35:47 +0200

    cleaned up tests

--- tests/visual_checks.py
+++ tests/visual_checks.py
@@ -1,24 +1,49 @@
 from invgp.kernels import orbits
-from invgp_experiments.datasets import load_mnist
 import matplotlib.pyplot as plt
 import tensorflow as tf
+from deepkernelinv_experiments.utils.load_datasets import load
+import tensorflow_datasets as tfds
+import numpy as np
 
-(X, Y), _ = load_mnist()
 
-# parametrization: [angle_deg, sx, sy, tx, ty], [0., 1., 1., 0., 0.]
+class args:
+    dataset = "CIFAR10"
+    image_shape = (32, 32, 3) if dataset == "CIFAR10" else (28, 28)
+    subset_size = None
+    orbit = 'affine'
 
-orbit = orbits.InterpretableSpatialTransform(
-    minibatch_size=10,
-    theta_min=[0., 0.5, 0.5, 0., 0., 0., 0.],
-    theta_max=[0., 2., 2., 0., 0., 0., 0.])
+
+train, test = load(args)
+X, _ = list(zip(*[a for a in tfds.as_numpy(train.take(7))]))
+X = np.stack(X).astype('float64')
+
+if args.orbit == 'colorspace':
+	orbit = orbits.ColorTransform(minibatch_size=10, log_lims_contrast=[-2., 2.], log_lims_brightness=[-2., 2.])
+if args.orbit == 'affine':
+	orbit = orbits.InterpretableSpatialTransform(
+	    minibatch_size=10,
+	    theta_max=[.5, 1.1, 1.1, 0., 0., 0., 0.],
+	    theta_min=[-.5, 0.9, 0.9, 0., 0., 0., 0.],
+	    colour=True,
+	    radians=True)
+
 Xo = orbit(X[:2, :])
 
-fig, ax = plt.subplots(11, 2)
+if args.dataset == "CIFAR10":
+	fig, ax = plt.subplots(11, 2)
+	ax[0, 0].imshow(tf.reshape(X[0], args.image_shape))
+	ax[0, 1].imshow(tf.reshape(X[1], args.image_shape))
+	for i in range(1, 11):
+		ax[i, 0].imshow(tf.reshape(Xo[0, i-1, :], args.image_shape))
+		ax[i, 1].imshow(tf.reshape(Xo[1, i-1, :], args.image_shape))
+	plt.show()
 
-ax[0, 0].imshow(tf.reshape(X[0], (28, 28)), cmap='gray')
-ax[0, 1].imshow(tf.reshape(X[1], (28, 28)), cmap='gray')
 
-for i in range(1, 11):
-    ax[i, 0].imshow(tf.reshape(Xo[0, i-1, :], (28, 28)), cmap='gray')
-    ax[i, 1].imshow(tf.reshape(Xo[1, i-1, :], (28, 28)), cmap='gray')
-plt.show()
+if args.dataset == "MNIST":
+	fig, ax = plt.subplots(11, 2)
+	ax[0, 0].imshow(tf.reshape(X[0], (28, 28)), cmap='gray', vmin=0, vmax=1)
+	ax[0, 1].imshow(tf.reshape(X[1], (28, 28)), cmap='gray', vmin=0, vmax=1)
+	for i in range(1, 11):
+		ax[i, 0].imshow(tf.reshape(Xo[0, i-1, :], (28, 28)), cmap='gray', vmin=0, vmax=1)
+		ax[i, 1].imshow(tf.reshape(Xo[1, i-1, :], (28, 28)), cmap='gray', vmin=0, vmax=1)
+	plt.show()
